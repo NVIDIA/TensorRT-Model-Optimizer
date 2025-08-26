d6325788b3afd83c2b8a71af6f9b4c9eca2bb136
Asha Anoosheh <aanoosheh@nvidia.com>
2025-08-26 06:23:25 -0700

    Squashed commit
    Signed-off-by: Asha Anoosheh <aanoosheh@nvidia.com>

--- examples/nemo_run/prune_distill/nemo_prune_kd_flow.py
+++ examples/nemo_run/prune_distill/nemo_prune_kd_flow.py
@@ -14,255 +14,315 @@
 # limitations under the License.
 
 import argparse
-from pathlib import Path
+import os
+import sys
+from datetime import timedelta
 
 import nemo_run as run
-from megatron.core.dist_checkpointing.validation import StrictHandling
 from nemo.collections import llm
-from nemo.collections.llm.api import export_ckpt
-from nemo.collections.llm.gpt.data import ChatDataModule, MockDataModule
+from nemo.collections.llm.gpt.data import MockDataModule, PreTrainingDataModule
 from nemo.collections.llm.modelopt.recipes.distillation_recipe import distillation_recipe
 from nemo.collections.llm.modelopt.recipes.prune_recipe import prune_recipe
 from nemo.collections.nlp.modules.common.tokenizer_utils import get_nmt_tokenizer
 
+from modelopt.torch.export.plugins.nemo_run import export_most_recent_ckpt
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "common")))
+from process_climbmix import SUBSET_IDX
+from utils import SlurmConfig, create_slurm_executor
+
 
 def get_args():
-    parser = argparse.ArgumentParser(
-        description="NeMo 2.0 Pruning + Distillation flow. Currently supports models that fit on 1 node and 8 GPUs."
-    )
+    parser = argparse.ArgumentParser(description="NeMo 2.0 Pruning + Distillation flow.")
     parser.add_argument(
-        "--experiment_name",
+        "--experiment",
         type=str,
         help="Experiment name",
         default="prune_distill_flow",
     )
     parser.add_argument(
-        "--model_hf",
+        "--model-name",
         type=str,
-        help="Hugging Face model name or path",
-        default="meta-llama/Meta-Llama-3.1-8B",
+        help="Name of the HF model",
+        default="Qwen/Qwen3-8B",
     )
     parser.add_argument(
-        "--recipe",
+        "--base-recipe",
         type=str,
-        default="llama31_8b",
+        default="qwen3_8b",
         help=(
             "Choose NeMo 2.0 recipe. Recipes are named in the format of "
             "<model_name>_<model_size>(_<long_sequence_length> or other special settings)"
         ),
     )
     parser.add_argument(
-        "--data_path",
+        "--hf-tokenizer",
         type=str,
-        help="Path to the finetuning chat dataset. Can be either ShareGPT or HuggingFace/OpenAI chat format",
+        help="Name of HF model to use for tokenizer.",
+        default="Qwen/Qwen3-8B",
     )
     parser.add_argument(
-        "--tokenizer_hf",
-        type=str,
-        help="Hugging Face tokenizer name or path",
-        default="meta-llama/Meta-Llama-3.1-8B-Instruct",
-    )
-    parser.add_argument(
-        "--chat_template",
-        type=str,
-        help="Path to the custom chat template to replace the HF tokenizer default chat template.",
-        required=False,
-    )
-    parser.add_argument(
-        "--prune_target_hidden_size",
+        "--prune-target-num-layers",
         type=int,
-        default=3072,
-        help="Target hidden size for pruning (also sets ffn_hidden_size to 3 * target_hidden_size)",
+        default=24,
+        help="Number of model layers to remain after pruning",
     )
     parser.add_argument(
-        "--mock_run",
+        "--log-dir",
+        type=str,
+        help=(
+            "Path to the directory to store logs. Best to pass in a non-relative path so that "
+            "artifacts are stored in one location."
+        ),
+        default="logs",
+    )
+    parser.add_argument(
+        "--data-dir",
+        type=str,
+        help="Path the preprocessed dataset",
+    )
+    parser.add_argument(
+        "--train-gpus",
+        type=int,
+        help="Number of GPUs for training",
+        default=8,
+    )
+    parser.add_argument(
+        "--nodes",
+        type=int,
+        help="Number of nodes",
+        default=1,
+    )
+    parser.add_argument(
+        "--use-slurm",
+        action="store_true",
+        help="Run on slurm using run.SlurmExecutor",
+    )
+    parser.add_argument(
+        "--mock-run",
         action="store_true",
         help="Run in mock mode",
     )
     return parser.parse_args()
 
 
-def get_finetune_recipe(recipe_name: str):
-    if not hasattr(getattr(llm, recipe_name), "finetune_recipe"):
-        raise ValueError(f"Recipe {recipe_name} does not have a Fine-Tuning recipe")
-    return getattr(llm, recipe_name).finetune_recipe(peft_scheme=None)
-
-
-def get_most_recent_subdir(directory: Path):
-    # Get all subdirectories
-    subdirs = [d for d in directory.iterdir() if d.is_dir()]
-    if not subdirs:
-        raise ValueError(f"No subdirectories found in {directory}")
-
-    # Sort by modification time (most recent first)
-    most_recent = max(subdirs, key=lambda x: x.stat().st_mtime)
-
-    return most_recent
-
-
-def get_most_recent_ckpt(directory: str):
-    """
-    Find the most recent checkpoint subdirectory in a given directory.
-
-    Args:
-        directory (str): Path to the directory to search in.
-
-    Returns:
-        str: Path to the most recent subdirectory.
-    """
-    exp_dir = Path(directory) / "default"
-    assert exp_dir.exists(), f"Experiment directory {exp_dir} does not exist"
-
-    checkpoint_dir = exp_dir / "checkpoints"
-    if checkpoint_dir.exists():
-        most_recent = get_most_recent_subdir(checkpoint_dir)
-    else:
-        most_recent = get_most_recent_subdir(exp_dir)
-        checkpoint_dir = most_recent / "checkpoints"
-        assert checkpoint_dir.exists(), f"Checkpoint directory {checkpoint_dir} does not exist"
-        most_recent = get_most_recent_subdir(checkpoint_dir)
-
-    return str(most_recent)
-
-
-def export_most_recent_ckpt(directory: str, output_path: str):
-    most_recent_ckpt = get_most_recent_ckpt(directory)
-    export_ckpt(most_recent_ckpt, "hf", output_path=output_path, overwrite=True)
-
-
-def _read_chat_template(template_path: str):
-    with open(template_path) as f:
-        return f.read().strip()
-
-
-if __name__ == "__main__":
-    args = get_args()
-
-    # # # # # CONFIGURABLE PARAMETERS # # # # #
-    GPUS = 8
-    SEQUENCE_LENGTH = 8192
-    MBS = 2
-    VAL_BATCHES = 32
-    if args.mock_run:
-        GBS = 8
-        FINETUNE_STEPS = 20
-        DISTILL_STEPS = 20
-        VAL_INTERVAL = 10
-        PRUNE_SAMPLES = 3
-        assert args.data_path is None, "Argument --data_path not used in mock mode."
-    else:
-        GBS = 2048
-        FINETUNE_STEPS = 2500
-        DISTILL_STEPS = 7500
-        VAL_INTERVAL = 500
-        PRUNE_SAMPLES = 1024
-        args.data_path = args.data_path if args.data_path is not None else "lima_processed"
-    # # # # # # # # # # # # # # # # # # # # # #
-
+def main(args):
     # Common items
-    model_name = args.recipe
-    model_module = getattr(llm, model_name)
+    exp_dir = os.path.join(args.log_dir, args.experiment)
+    model_name = args.base_recipe
+
     if args.mock_run:
         data = run.Config(
             MockDataModule,
-            global_batch_size=GBS,
-            micro_batch_size=MBS,
+            global_batch_size=DISTILL_GBS,
+            micro_batch_size=DISTILL_MBS,
             seq_length=SEQUENCE_LENGTH,
         )
     else:
         tokenizer = run.Config(
             get_nmt_tokenizer,
             library="huggingface",
-            model_name=args.tokenizer_hf,
-            chat_template=_read_chat_template(args.chat_template) if args.chat_template else None,
+            model_name=args.hf_tokenizer,
         )
         data = run.Config(
-            ChatDataModule,
-            dataset_root=args.data_path,
+            PreTrainingDataModule,
+            paths=[f"{args.data_dir}/part_{i}_text_document" for i in SUBSET_IDX],
             seq_length=SEQUENCE_LENGTH,
             tokenizer=tokenizer,
-            global_batch_size=GBS,
-            micro_batch_size=MBS,
-            use_hf_tokenizer_chat_template=True,
+            global_batch_size=DISTILL_GBS,
+            micro_batch_size=DISTILL_MBS,
+            split="99,1,0",
         )
 
-    # 1. Process data and Import and save initial NeMo checkpoint
-    lima_data = run.Script("process_lima.py", entrypoint="python")
-
-    initial_model_out = f"{args.experiment_name}/{model_name}_initial"
+    # 1. Import and save initial NeMo checkpoint
+    initial_model_out = f"{exp_dir}/{model_name}_initial"
+    model_module = getattr(llm, model_name)
     import_model = run.Partial(
         llm.import_ckpt,
         model=model_module.model(),
-        source=f"hf://{args.model_hf}",
+        source=f"hf://{args.model_name}",
         output_path=initial_model_out,
+        overwrite=True,
     )
 
-    # 2. Finetune
-    finetune = get_finetune_recipe(args.recipe)
-    finetune.tokenizer = "data"
-    finetune.data = data
-    finetune.resume.restore_config.path = initial_model_out
-    finetune.log.log_dir = f"{args.experiment_name}/finetune_log_dir"
-    finetune.trainer.strategy.tensor_model_parallel_size = GPUS
-    finetune.trainer.max_steps = FINETUNE_STEPS
-    finetune.trainer.val_check_interval = VAL_INTERVAL
-    finetune.trainer.limit_val_batches = VAL_BATCHES
-
-    # 3. Prune to obtain student
+    # 2. Prune to obtain student
     prune_data = data.clone()
+    prune_data.micro_batch_size = PRUNE_MBS
     prune_data.global_batch_size = prune_data.micro_batch_size
-    finetuned_teacher_path = run.Config(get_most_recent_ckpt, finetune.log.log_dir)
-    pruned_model_out = f"{args.experiment_name}/{model_name}_pruned"
+    pruned_model_out = f"{exp_dir}/{model_name}_pruned"
     prune = prune_recipe(
-        nemo_checkpoint=finetuned_teacher_path,
+        nemo_checkpoint=initial_model_out,
         save_path=pruned_model_out,
     )
-    prune.pruning_config.target_hidden_size = args.prune_target_hidden_size
-    prune.pruning_config.target_ffn_hidden_size = args.prune_target_hidden_size * 3
-    prune.devices = GPUS
-    prune.pp_size = GPUS
+    prune.tokenizer_path = args.hf_tokenizer
+    prune.pruning_config.target_num_layers = args.prune_target_num_layers
+    prune.devices = 1
+    prune.pp_size = 1
     prune.data = prune_data
     prune.num_train_samples = PRUNE_SAMPLES
     prune.legacy_ckpt = True
 
-    # 4. Distill
+    # 3. Distill
     distill = distillation_recipe(
-        teacher_model_path=finetuned_teacher_path,
+        teacher_model_path=initial_model_out,
         student_model_path=pruned_model_out,
+        num_nodes=args.nodes,
+        num_gpus_per_node=args.train_gpus,
     )
     distill.data = data
     distill.tokenizer = "data"
-    distill.log.log_dir = f"{args.experiment_name}/distill_log_dir"
+    distill.log.log_dir = f"{exp_dir}/distill_log_dir"
+    distill.log.ckpt.train_time_interval = run.Config(timedelta, hours=SAVE_CKPT_EVERY_N_HOURS)
+    distill.log.ckpt.save_top_k = 2
+    distill.optim.config.lr = MAX_LR
+    distill.optim.lr_scheduler.min_lr = MIN_LR
+    distill.optim.lr_scheduler.warmup_steps = WARMUP_STEPS
     distill.trainer.max_steps = DISTILL_STEPS
     distill.trainer.val_check_interval = VAL_INTERVAL
     distill.trainer.limit_val_batches = VAL_BATCHES
-    distill.trainer.strategy.ckpt_load_strictness = StrictHandling.LOG_ALL
+    distill.trainer.strategy.tensor_model_parallel_size = args.train_gpus
+    distill.trainer.strategy.ckpt_load_strictness = "log_all"
+
+    # 4. Evaluate MMLU
+    if args.use_slurm:
+        mmlu_script_path = "examples/nemo_run/common/in_memory_mmlu.py"
+    else:
+        mmlu_script_path = os.path.abspath(
+            os.path.join(os.path.dirname(__file__), "../common/in_memory_mmlu.py")
+        )
+    eval_teacher = run.Script(
+        mmlu_script_path,
+        entrypoint="python",
+        args=["--nemo_ckpt", initial_model_out, "--tensor_parallelism", f"{args.train_gpus}"],
+    )
+    eval_student = run.Script(
+        mmlu_script_path,
+        entrypoint="python",
+        args=[
+            "--finetuned_ckpt_dir",
+            distill.log.log_dir,
+            "--tensor_parallelism",
+            f"{args.train_gpus}",
+        ],
+    )
 
     # 5. Export
-    export_path = f"{args.experiment_name}/{model_name}_final"
+    export_path = f"{exp_dir}/{model_name}_final"
     export_model = run.Partial(
         export_most_recent_ckpt,
-        directory=f"{args.experiment_name}/distill_log_dir",
+        directory=distill.log.log_dir,
         output_path=export_path,
     )
 
-    # Run all
-    executor_single = run.LocalExecutor()
-    executor_multi = run.LocalExecutor(launcher="torchrun", ntasks_per_node=GPUS)
-    with run.Experiment(args.experiment_name, log_level="INFO") as exp:
-        if args.data_path == "lima_processed":
-            s0 = exp.add(lima_data, tail_logs=True, name="lima_data", executor=run.LocalExecutor())
-        s1 = exp.add(import_model, executor=executor_single, tail_logs=True, name="import")
+    # Setup executors
+    if args.use_slurm:
+        gpu_executor = create_slurm_executor(
+            SLURM_CONFIG,
+            ntasks_per_node=1,
+            num_gpus=1,
+            nodes=1,
+        )
+        multi_gpu_executor = create_slurm_executor(
+            SLURM_CONFIG,
+            ntasks_per_node=args.train_gpus,
+            num_gpus=args.train_gpus,
+            nodes=args.nodes,
+        )
+    else:
+        gpu_executor = run.LocalExecutor(launcher="torchrun", ntasks_per_node=1)
+        multi_gpu_executor = run.LocalExecutor(launcher="torchrun", ntasks_per_node=args.train_gpus)
+
+    # Execute
+    with run.Experiment(exp_dir, log_level="INFO") as exp:
+        s1 = exp.add(
+            import_model,
+            executor=gpu_executor,
+            tail_logs=True,
+            name="01_import",
+        )
         s2 = exp.add(
-            finetune, executor=executor_multi, tail_logs=True, name="finetune", dependencies=[s1]
+            prune,
+            executor=gpu_executor,
+            tail_logs=True,
+            name="02_prune",
+            dependencies=[s1],
         )
         s3 = exp.add(
-            prune, executor=executor_multi, tail_logs=True, name="prune", dependencies=[s2]
+            distill,
+            executor=multi_gpu_executor,
+            tail_logs=True,
+            name="03_distill",
+            dependencies=[s2],
         )
-        s4 = exp.add(
-            distill, executor=executor_multi, tail_logs=True, name="distill", dependencies=[s3]
+        _ = exp.add(
+            eval_teacher,
+            executor=multi_gpu_executor,
+            tail_logs=True,
+            name="eval_teacher",
+            dependencies=[s1],
         )
-        s5 = exp.add(
-            export_model, executor=executor_single, tail_logs=True, name="export", dependencies=[s4]
+        _ = exp.add(
+            eval_student,
+            executor=multi_gpu_executor,
+            tail_logs=True,
+            name="eval_student",
+            dependencies=[s3],
         )
-        exp.run()
+        # WAR: Export needs access to all GPUs but only 1 task due to bug in NeMo
+        multi_gpu_executor.ntasks_per_node = 1  # will throw error if more than 1 task during export
+        _ = exp.add(
+            export_model,
+            executor=multi_gpu_executor,
+            tail_logs=True,
+            name="04_export",
+            dependencies=[s3],
+        )
+        exp.run(detach=True)
+
+
+if __name__ == "__main__":
+    args = get_args()
+
+    # # # # # # # # SLURM SETUP # # # # # #
+    # # # # # # # MODIFY THIS # # # # # # #
+    if args.use_slurm:
+        SLURM_CONFIG = SlurmConfig(
+            account="",
+            partition_gpu="batch",
+            partition_cpu="cpu",
+            time="HH:MM:SS",
+            container_image="nvcr.io/nvidia/nemo:25.09",
+            env_vars={
+                "HF_TOKEN": "",
+            },
+            use_local_tunnel=False,
+            host="",
+            user="",
+            container_mounts=[],
+            job_dir="/path/to/logs",
+        )
+
+    # # # # # # # # # # # # # # # # # # # # # #
+    # # # # # CONFIGURABLE PARAMETERS # # # # #
+    SEQUENCE_LENGTH = 8192
+    PRUNE_MBS = 4
+    DISTILL_MBS = 2
+    VAL_BATCHES = 32
+    MAX_LR = 1e-4
+    MIN_LR = 1e-5
+    WARMUP_STEPS = 100
+    SAVE_CKPT_EVERY_N_HOURS = 3.5
+    if args.mock_run:
+        PRUNE_SAMPLES = 3
+        DISTILL_GBS = 8
+        DISTILL_STEPS = 20
+        VAL_INTERVAL = 10
+    else:
+        PRUNE_SAMPLES = 1024
+        DISTILL_GBS = 768
+        _NUM_TOKENS = 89694564352
+        DISTILL_STEPS = int(_NUM_TOKENS / DISTILL_GBS / SEQUENCE_LENGTH)
+        VAL_INTERVAL = 1000
+    # # # # # # # # # # # # # # # # # # # # # #
+
+    main(args)
