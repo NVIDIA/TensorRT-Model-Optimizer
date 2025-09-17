3b3d08b8ff0c3a715601ab5303b3c3bf535572d8
Chenjie Luo <chenjiel@nvidia.com>
2025-09-17 16:02:39 +0000

    Disable t5 test. TRT LLM does not support t5 hf checkpoint
    Signed-off-by: Chenjie Luo <chenjiel@nvidia.com>

--- tests/gpu/torch/export/test_unified_hf_export_and_check_safetensors.py
+++ tests/gpu/torch/export/test_unified_hf_export_and_check_safetensors.py
@@ -38,7 +38,6 @@ from safetensors import safe_open
         ("nvfp4_awq", "tiny_llama-nvfp4-awq", True, False, True, True),
         ("int4_awq", "tiny_llama-int4-awq", True, False, True, True),
         ("w4a8_awq", "tiny_llama-w4a8-awq", True, False, True, True),
-        ("fp8", "t5_tiny-fp8", True, False, True, True),
     ],
 )
 def test_unified_hf_export_and_check_safetensors(
