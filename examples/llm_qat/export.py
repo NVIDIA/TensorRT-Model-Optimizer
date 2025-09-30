ba7ffabe5e735149015e2aba96daae7bb095cd91
Suguna Velury <178320438+sugunav14@users.noreply.github.com>
2025-09-30 16:21:31 +0000

    minor
    Signed-off-by: Suguna Velury <178320438+sugunav14@users.noreply.github.com>

--- examples/llm_qat/export.py
+++ examples/llm_qat/export.py
@@ -84,7 +84,6 @@ def main(args):
 
         config_path = f"{export_dir}/base_model/config.json"
 
-        # In the case of LoRA model.save_pretrained does not save the correct config.json
         config_data = model.config.to_dict()
 
         config_data["quantization_config"] = hf_quant_config
