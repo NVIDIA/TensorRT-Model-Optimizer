872fd6c5d45b84d1acf21ede036664c9e3de7aac
Chenhan Yu <chenhany@nvidia.com>
2025-10-06 13:09:47 -0700

    chg: update

--- modelopt/torch/utils/plugins/trasnformers_datasets.py
+++ modelopt/torch/utils/plugins/trasnformers_datasets.py
@@ -1,6 +1,7 @@
 """Processing large data to tokenize for pretraining."""
 
-import warnings
+import copy
+import itertools
 
 import torch
 import transformers
@@ -30,38 +31,73 @@ def _sharegpt_to_openai_messages(conversations: list[dict]):
     return messages
 
 
-class LanguageDataset(torch.utils.data.Dataset):
-    """LanguageDataset with on-the-fly tokenization over HuggingFace datasets.
+class ShardedDataset(torch.utils.data.Dataset):
+    """ShardedDataset is a subclass of torch.utils.data.Dataset that is used to load data from a dataset."""
 
-    Args:
-        tokenizer: The tokenizer to use.
-        data_path: The path to the dataset.
-        subset: The subset to use.
-        split: The split to use.
-        max_length: The maximum length of the sequence.
-        num_shards: The number of shards to use.
-        shard_index: The index of the shard to use.
-        chat_template: The chat template to use.
-        add_generation_prompt: Whether to add a generation prompt.
-        json_key: The key to use to get the text from the dataset.
-        answer_only_loss: Whether to use answer-only loss.
-        streaming: Whether to use streaming.
-    """
+    def __init__(
+        self,
+        name: str,
+        subset: str | None = None,
+        split: str = "train",
+        num_shards: int = 1,
+        shard_index: int = 0,
+        num_streaming_samples: int | None = None,
+    ):
+        """Initialize the ShardedDataset."""
+        self.name = name
+        self.subset = subset
+        self.split = split
+        self.num_shards = num_shards
+        self.shard_index = shard_index
+        self.num_streaming_samples = num_streaming_samples
+
+        self._load_dataset()
+
+    def __len__(self):
+        if self.num_streaming_samples is not None:
+            return self.num_streaming_samples
+        else:
+            return len(self._raw_samples)
+
+    def __getitem__(self, index):
+        index = index // self.num_shards
+
+        if self.num_streaming_samples is not None:
+            while index >= len(self._raw_samples):
+                self._raw_samples.append(next(self._stream_iterator))
+
+        return self._raw_samples[index]
+
+    def _load_dataset(self):
+        dataset = load_dataset(
+            self.name,
+            self.subset,
+            split=self.split,
+            # num_proc=4,  # TODO: Make this configurable
+            streaming=self.num_streaming_samples is not None,
+        )
+
+        shard = dataset.shard(num_shards=self.num_shards, index=self.shard_index)
+
+        if self.num_streaming_samples is not None:
+            self._raw_samples = []
+            self._stream_samples = shard
+            self._stream_iterator = itertools.cycle(self._stream_samples)
+        else:
+            self._raw_samples = shard
+
+
+class LanguageDataCollator:
+    """LanguageDataCollator is a class that is used to collate language data."""
 
     def __init__(
         self,
         tokenizer: transformers.PreTrainedTokenizerBase,
-        data_path: str,
-        subset: str | None = None,
-        split: str = "train",
         max_length: int = 4096,
-        num_shards: int = 1,
-        shard_index: int = 0,
         chat_template: str | None = None,
         add_generation_prompt: bool = False,
-        json_key: str = "text",
         answer_only_loss: bool = False,
-        streaming: bool = True,
+        json_key: str = "text",
     ):
         """Initialize the LanguageDataset."""
         if not isinstance(tokenizer, transformers.PreTrainedTokenizerBase):
@@ -70,19 +106,11 @@ class LanguageDataset(torch.utils.data.Dataset):
                     type(tokenizer)
                 )
             )
-
         self.tokenizer = tokenizer
-        self.data_path = data_path
-        self.subset = subset
-        self.split = split
         self.max_length = max_length
-        self.num_shards = num_shards
-        self.shard_index = shard_index
         self.add_generation_prompt = add_generation_prompt
-        self.json_key = json_key
         self.answer_only_loss = answer_only_loss
-        self.streaming = streaming
-        self._post_process_tokenizer()
+        self.json_key = json_key
 
         if chat_template is not None:
             self.tokenizer.chat_template = chat_template
@@ -91,35 +119,6 @@ class LanguageDataset(torch.utils.data.Dataset):
 
         if self.tokenizer.chat_template is None:
             raise ValueError("No valid chat template!")
-
-        if self.answer_only_loss and "{% generation %}" not in self.tokenizer.chat_template:
-            warnings.warn(
-                "The chat template does not contain the {% generation %} tag. OVERWRITE answer_only_loss to False!"
-            )
-            self.answer_only_loss = False
-
-        if self.tokenizer.padding_side != "right":
-            warnings.warn(
-                "The tokenizer padding side is {}. OVERWRITE to right!".format(
-                    self.tokenizer.padding_side
-                )
-            )
-            self.tokenizer.padding_side = "right"
-
-        self._load_dataset()
-
-    def __len__(self):
-        return len(self._raw_samples)
-
-    def __getitem__(self, index):
-        index = index // self.num_shards
-
-        if self.streaming:
-            while index >= len(self._raw_samples):
-                self._raw_samples.append(next(self._iterator))
-
-        raw_sample = self._raw_samples[index]
-        return self._process_and_pack_sample(raw_sample)
 
     def _post_process_tokenizer(self):
         if hasattr(self.tokenizer, "pad_token") and self.tokenizer.pad_token is None:
@@ -135,33 +134,9 @@ class LanguageDataset(torch.utils.data.Dataset):
             REMOVE_THINK_CHAT_TEMPLATE, ""
         )
 
-    def _load_dataset(self):
-        self._raw_samples = []
-        self._stream_samples = load_dataset(
-            self.data_path,
-            self.subset,
-            split=self.split,
-            # num_proc=4,  # TODO: Make this configurable
-            streaming=True,
-        )
-        self._steam_samples = self._stream_samples.shard(
-            num_shards=self.num_shards, index=self.shard_index
-        )
-        self._iterator = iter(self._stream_samples)
-
-    def _process_chat_sample(self, example):
-        messages = example.get("messages", None)
-        if messages is None:
-            conversations = example.get("conversations", None)
-            if conversations is None:
-                raise ValueError(
-                    "The sample must in either OpenAI messages format or ShareGPT conversations format."
-                )
-            else:
-                messages = _sharegpt_to_openai_messages(conversations)
-
-        tokenized_messages = self.tokenizer.apply_chat_template(
-            messages,
+    def _process_chat_sample(self, examples: list):
+        tokenized_examples = self.tokenizer.apply_chat_template(
+            examples,
             return_tensors="pt",
             return_dict=True,
             padding="max_length",
@@ -170,58 +145,98 @@ class LanguageDataset(torch.utils.data.Dataset):
             add_generation_prompt=self.add_generation_prompt,
             return_assistant_tokens_mask=self.answer_only_loss,
         )
-        return tokenized_messages
+        return tokenized_examples
 
-    def _process_text_sample(self, text: str):
-        tokenized_sample = self.tokenizer(
-            text,
+    def _process_text_sample(self, examples: list):
+        tokenized_examples = self.tokenizer(
+            examples,
             return_tensors="pt",
             padding="max_length",
             truncation=True,
             max_length=self.max_length,
         )
-        return tokenized_sample
+        return tokenized_examples
 
-    def _process_and_pack_sample(self, example):
-        if not isinstance(example, dict):
-            raise ValueError("The sample must be a Dict but got {}".format(type(example)))
-        text = example.get(self.json_key, None)
-        if isinstance(text, str):
-            return self._process_text_sample(text)
-        return self._process_chat_sample(example)
+    def __call__(self, examples):
+        """Call the LanguageDataCollator."""
+        batch = []
+
+        for example in examples:
+            if not isinstance(example, dict):
+                raise ValueError("The sample must be a Dict but got {}".format(type(example)))
+            text = example.get(self.json_key, None)
+            if isinstance(text, str):
+                batch.append(text)
+            else:
+                messages = example.get("messages", None)
+                if messages is None:
+                    conversations = example.get("conversations", None)
+                    if conversations is None:
+                        raise ValueError(
+                            "The sample must in either OpenAI messages format or ShareGPT conversations format."
+                        )
+                    else:
+                        messages = _sharegpt_to_openai_messages(conversations)
+                batch.append(messages)
+
+        return self._process_chat_sample(batch)
 
 
-class VisionLanguageDataset(LanguageDataset):
-    """VisionLanguageDataset with on-the-fly tokenization over HuggingFace datasets.
+class LanguageDataset(ShardedDataset):
+    """LanguageDataset is a subclass of ShardedDataset that is used to load language data."""
 
-    Args:
-        processor: The processor to use.
-        data_path: The path to the dataset.
-        subset: The subset to use.
-        split: The split to use.
-        max_length: The maximum length of the sequence.
-        num_shards: The number of shards to use.
-        shard_index: The index of the shard to use.
-        chat_template: The chat template to use.
-        add_generation_prompt: Whether to add a generation prompt.
-        answer_only_loss: Whether to use answer-only loss.
-        streaming: Whether to use streaming.
-        local_image_path: The path to the local images.
-    """
+    def __init__(
+        self,
+        tokenizer: transformers.PreTrainedTokenizerBase,
+        name: str,
+        subset: str | None = None,
+        split: str = "train",
+        num_shards: int = 1,
+        shard_index: int = 0,
+        max_length: int = 4096,
+        chat_template: str | None = None,
+        add_generation_prompt: bool = False,
+        answer_only_loss: bool = False,
+        json_key: str = "text",
+    ):
+        """Initialize the LanguageDataset."""
+        super().__init__(
+            name=name,
+            subset=subset,
+            split=split,
+            num_shards=num_shards,
+            shard_index=shard_index,
+        )
+        self.collator = LanguageDataCollator(
+            tokenizer=tokenizer,
+            max_length=max_length,
+            chat_template=chat_template,
+            add_generation_prompt=add_generation_prompt,
+            answer_only_loss=answer_only_loss,
+            json_key=json_key,
+        )
+
+    def __getitem__(self, index):
+        """Get the item at the given index."""
+        index = index // self.num_shards
+
+        if self.num_streaming_samples is not None:
+            while index >= len(self._raw_samples):
+                self._raw_samples.append(next(self._stream_iterator))
+
+        return self.collator([self._raw_samples[index]])
+
+
+class VisionLanguageDataCollator(LanguageDataCollator):
+    """VisionLanguageDataCollator is a subclass of LanguageDataCollator that is used to collate vision-language data."""
 
     def __init__(
         self,
         processor: transformers.ProcessorMixin,
-        data_path: str,
-        subset: str | None = None,
-        split: str = "train",
-        max_length: int = 4096,
-        num_shards: int = 1,
-        shard_index: int = 0,
+        max_length: int = 8192,
         chat_template: str | None = None,
         add_generation_prompt: bool = False,
         answer_only_loss: bool = False,
-        streaming: bool = True,
         local_image_path: str | None = None,
     ):
         """Initialize the VisionLanguageDataset."""
@@ -233,45 +248,23 @@ class VisionLanguageDataset(LanguageDataset):
             )
 
         self.processor = processor
+        self.max_length = max_length
+        self.chat_template = chat_template
+        self.add_generation_prompt = add_generation_prompt
+        self.answer_only_loss = answer_only_loss
         self.local_image_path = local_image_path
-        if self.local_image_path is None:
-            self.local_image_path = data_path + "/images"
 
         super().__init__(
             tokenizer=self.processor.tokenizer,
-            data_path=data_path,
-            subset=subset,
-            split=split,
             max_length=max_length,
-            num_shards=num_shards,
-            shard_index=shard_index,
             chat_template=chat_template,
             add_generation_prompt=add_generation_prompt,
             answer_only_loss=answer_only_loss,
-            streaming=streaming,
         )
 
-    def _process_multimodal_sample(self, example):
-        messages = example.get("messages", None)
-        if messages is None:
-            print(example)
-            conversations = example.get("conversations", None)
-            if conversations is None:
-                raise ValueError(
-                    "The sample must in either OpenAI messages format or ShareGPT conversations format."
-                )
-            else:
-                messages = _sharegpt_to_openai_messages(conversations)
-
-        for msg in messages:
-            if isinstance(msg["content"], str):
-                msg["content"] = [{"type": "text", "text": msg["content"]}]
-            for ctn in msg["content"]:
-                if ctn["type"] == "image" and "path" in ctn:
-                    ctn["path"] = self.local_image_path + "/" + ctn["path"]
-
+    def _process_multimodal_sample(self, examples):
         tokenized_messages = self.processor.apply_chat_template(
-            messages,
+            examples,
             tokenize=True,
             return_tensors="pt",
             return_dict=True,
@@ -283,8 +276,31 @@ class VisionLanguageDataset(LanguageDataset):
         )
         return tokenized_messages
 
-    def _process_and_pack_sample(self, example):
-        if not isinstance(example, dict):
-            raise ValueError("The sample must be a Dict but got {}".format(type(example)))
+    def __call__(self, examples):
+        """Call the VisionLanguageDataCollator."""
+        batch = []
 
-        return self._process_multimodal_sample(example)
+        for example in examples:
+            messages = example.get("messages", None)
+            if messages is None:
+                # print(example)
+                conversations = example.get("conversations", None)
+                if conversations is None:
+                    raise ValueError(
+                        "The sample must in either OpenAI messages format or ShareGPT conversations format."
+                    )
+                else:
+                    messages = _sharegpt_to_openai_messages(conversations)
+
+            copy_messages = copy.deepcopy(messages)
+
+            for msg in copy_messages:
+                if isinstance(msg["content"], str):
+                    msg["content"] = [{"type": "text", "text": msg["content"]}]
+                for ctn in msg["content"]:
+                    if ctn["type"] == "image" and "path" in ctn:
+                        ctn["path"] = self.local_image_path + "/" + ctn["path"]
+
+            batch.append(copy_messages)
+
+        return self._process_multimodal_sample(batch)
