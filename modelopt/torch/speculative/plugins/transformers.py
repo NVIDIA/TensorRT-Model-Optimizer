171b2301ef7c9159a8ee80060e982601c6256ea2
h-guo18 <67671475+h-guo18@users.noreply.github.com>
2025-10-17 00:40:28 +0000

    squash: add trainer
    Signed-off-by: h-guo18 <67671475+h-guo18@users.noreply.github.com>

--- modelopt/torch/speculative/plugins/transformers.py
+++ modelopt/torch/speculative/plugins/transformers.py
@@ -586,10 +586,10 @@ class HFEagleModel(EagleModel):
         self,
         input_ids,
         attention_mask,
-        position_ids,
-        past_key_values,
-        freeze_base_model,
-        labels,
+        position_ids=None,
+        past_key_values=None,
+        freeze_base_model=True,
+        labels=None,
         **kwargs,
     ):
         # TODO: This function still use eagle_module. Ideally we should remove it,
@@ -726,7 +726,7 @@ class HFEagleModel(EagleModel):
 
         # ====Run eagle forward====
         eagle_loss = None
-        train_accs = []
+        train_accs = {}
         if self.training:
             # In EAGLE-3, we have an additional FC layer to concentrate hidden states from multiple base model layers
             b, seq_length, h = base_model_hidden_states.shape
@@ -770,7 +770,7 @@ class HFEagleModel(EagleModel):
                 loss_mask[:, 1:],
             )
             eagle_loss = classification_loss
-            train_accs.append(acc)
+            train_accs["train_acc_step0"] = acc
 
             # ====Perform training-time-testing with 3 extra eagle forward passes====
             for ttt_step in range(self.num_ttt_steps):
@@ -811,7 +811,7 @@ class HFEagleModel(EagleModel):
                     ),
                 )
                 eagle_loss += classification_loss
-                train_accs.append(acc)
+                train_accs[f"train_acc_step{ttt_step + 1}"] = acc
         # Finally, we merge base model loss and eagle loss, raise error if both are None
         if base_model_loss is not None and eagle_loss is not None:
             loss = base_model_loss + eagle_loss
@@ -830,7 +830,7 @@ class HFEagleModel(EagleModel):
             logits=base_model_logits,
             past_key_values=past_key_values,
             hidden_states=base_model_hidden_states,
-            train_acc=train_accs,
+            **train_accs,
         )
 
     def _eagle_loss(
