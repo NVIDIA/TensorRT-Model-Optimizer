44a52675b54c24447d91f07736aee7ecbeaf4916
jenchen13 <jennifchen@nvidia.com>
2025-11-14 09:29:27 -0800

    add nvfp4
    Signed-off-by: jenchen13 <jennifchen@nvidia.com>

--- modelopt/torch/export/unified_export_megatron.py
+++ modelopt/torch/export/unified_export_megatron.py
@@ -38,6 +38,7 @@ from modelopt.torch.utils import import_plugin
 
 from .model_config import (
     KV_CACHE_FP8,
+    KV_CACHE_NVFP4,
     QUANTIZATION_FP8,
     QUANTIZATION_FP8_PB_REAL,
     QUANTIZATION_FP8_PB_WO,
@@ -338,9 +339,10 @@ class GPTModelExporter:
             quantization = "NVFP4"
 
         kv_cache_quantization = None
-        if get_kv_cache_dtype(self.model) == KV_CACHE_FP8:
+        kv_cache_dtype = get_kv_cache_dtype(self.model)
+        if  kv_cache_dtype == KV_CACHE_FP8 or kv_cache_dtype == KV_CACHE_NVFP4:
             # Only FP8 KV Cache is supported in VLLM for now
-            kv_cache_quantization = "FP8"
+            kv_cache_quantization = kv_cache_dtype
         # We use the last PP rank and the 1st EP rank to write the config because
         # medusa_heads and eagle_module only exist in the last stage.
         if is_last_stage_main_rank:
