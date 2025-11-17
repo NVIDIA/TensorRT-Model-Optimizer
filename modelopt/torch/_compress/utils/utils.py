f4de1714fe97c89f82ca1c48dd09c54ef8052133
Daniel Korzekwa <dkorzekwa@nvidia.com>
2025-11-17 19:54:05 +0100

    Regenerate licence header
    Signed-off-by: Daniel Korzekwa <dkorzekwa@nvidia.com>

--- modelopt/torch/_compress/utils/utils.py
+++ modelopt/torch/_compress/utils/utils.py
@@ -13,18 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
-# SPDX-License-Identifier: LicenseRef-NvidiaProprietary
-#
-# NVIDIA CORPORATION, its affiliates and licensors retain all intellectual
-# property and proprietary rights in and to this material, related
-# documentation and any modifications thereto. Any use, reproduction,
-# disclosure or distribution of this material and related documentation
-# without an express license agreement from NVIDIA CORPORATION or
-# its affiliates is strictly prohibited.
-# mypy: ignore-errors
-
-
 import torch
 
 
