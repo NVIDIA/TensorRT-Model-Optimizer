c333d36a851fb11ce2f4585b2749ad86b3c269dd
Keval Morabia <28916987+kevalmorabia97@users.noreply.github.com>
2025-11-19 17:11:09 +0530

    Enable torch 2.9 tests in CICD + Diffusers fixes (#561)
    As title
    
    ---------
    
    Signed-off-by: Keval Morabia <28916987+kevalmorabia97@users.noreply.github.com>
    Signed-off-by: ajrasane <131806219+ajrasane@users.noreply.github.com>
    Co-authored-by: ajrasane <131806219+ajrasane@users.noreply.github.com>

--- modelopt/torch/quantization/export_onnx.py
+++ modelopt/torch/quantization/export_onnx.py
@@ -169,7 +169,7 @@ def export_int8(
     scale.masked_fill_(scale == 0, 1.0)
     scale = g.op("Constant", value_t=scale)
 
-    assert trt_high_precision_dtype in (input_type, "Float"), (
+    assert trt_high_precision_dtype in (input_type, "Float", "BFloat16"), (
         "TRT StronglyType requires both weights and amax to be in the BF16/FP16, or the QDQ in Float."
     )
 
@@ -410,7 +410,7 @@ def export_fp8_mha(
     if hasattr(torch.onnx, "_type_utils"):
         from torch.onnx._type_utils import JitScalarType
     else:  # torch >= 2.9
-        from torch.onnx._internal.torchscript_exporter import JitScalarType
+        from torch.onnx._internal.torchscript_exporter._type_utils import JitScalarType
 
     if hasattr(torch.onnx, "symbolic_opset14"):
         from torch.onnx.symbolic_opset14 import _attention_scale, _causal_attention_mask
